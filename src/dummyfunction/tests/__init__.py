043e168a3e6e624d05cdcf3f42695fc15e9887e2
Johannes <johannes.programming@gmail.com>
2025-08-22 20:17:49 +0200

    Version 1.0.5

--- src/dummyfunction/tests/__init__.py
+++ src/dummyfunction/tests/__init__.py
@@ -3,8 +3,12 @@ import unittest
 __all__ = ["test"]
 
 
-def test() -> unittest.TextTestRunner:
+def test() -> unittest.TextTestResult:
     "This function runs all the tests."
+    loader: unittest.TestLoader
+    tests: unittest.TestSuite
+    runner: unittest.TextTestRunner
+    result: unittest.TextTestResult
     loader = unittest.TestLoader()
     tests = loader.discover(start_dir="dummyfunction.tests")
     runner = unittest.TextTestRunner()
